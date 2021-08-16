4294c2e6e548c112494e6ca8eb2e86adfdb486ef
楊翔宇 <73743919+racercode@users.noreply.github.com>
2021-08-16 11:57:08 +0800

    已完整產生需要之餐廳

--- get_foodpanda.py
+++ get_foodpanda.py
@@ -1,13 +1,14 @@
 import requests as rq
 import json
 import os
+from dislash import SelectMenu, SelectOption, InteractionClient
 import discord as dc
 import asyncio
 from discord.ext import commands
 from dotenv import load_dotenv
 
 bot=commands.Bot(command_prefix='##')
-command_prefix = '##'
+slash = InteractionClient(bot)
 
 payload={"q":"starbucks","location":{"point":{"latitude":22.6394924,"longitude":120.302583}},"config":"Variant21","customer_id  ":"","vertical_types":["restaurants"],"include_component_types":["vendors"],"include_fields":["feed"],"language_id":"6","opening    _type":"delivery","platform":"web","session_id":"","language_code":"zh","customer_type":"regular","limit":10,"offset":0,"dps_se  ssion_id":"eyJzZXNzaW9uX2lkIjoiMzhhOGUwZTBmYTllZmNlMGIzZTM4ZjNkZDFiMzE4ZjkiLCJwZXJzZXVzX2lkIjoiMTYyODg0ODk0NC42NTQzNzg4MjU2LjQxe    E05NHIxSDgiLCJ0aW1lc3RhbXAiOjE2Mjg5NDYwMDV9","dynamic_pricing":0,"brand":"foodpanda","country_code":"tw","use_free_delivery_la  bel":False}
 
@@ -23,26 +24,24 @@ async def on_ready():
 used=False
 entered=False
 keyword=''
-
 @bot.event
 async def on_message(message):
     string=message.content
-
-    if message.content == command_prefix+"echo":
-        await message.channel.send('echo')
-
-    if string.startswith('init') :
+    if string.startswith('##init') :
         string,a,b=string.split(' ')
         global payload
         global entered
+        global used
         payload['location']['point']['latitude']=float(a)
         payload['location']['point']['longitude']=float(b)
         entered=True
         await message.channel.send('定位成功 !')
-
-    elif string.startswith('search') :
-        global used
+    elif string.startswith('##clear') :
+        used=False
+        await message.channel.send('查詢資料清除成功')
+    elif string.startswith('##search') :
         global keyword
+        global reply
 
         if entered==False :
             await message.channel.send('請先輸入座標')
@@ -50,13 +49,12 @@ async def on_message(message):
             await message.channel.send('已經開始一個查詢')
         else :
             used=True
-            string,a=string.split(' ')
-            keyword=a
+            string,keyword=string.split(' ')
 
             now_payload=payload
             now_payload['q']=keyword
             r=rq.post('https://disco.deliveryhero.io/search/api/v1/feed',data=json.dumps(now_payload),headers=headers)
-            print(r.text)
+            
             rt_list=json.loads(r.text)
             r_list=rt_list['feed']['items'][0]['items']
         
@@ -64,13 +62,33 @@ async def on_message(message):
                 title='請選擇所要的餐廳'
             )
             for i in range(1,6) :
-                embed.add_field(name=f'{i}. {r_list[i-1]["name"]}',value=f'評價{r_list[i-1]["rating"]}顆星',inline=False)
-
+                embed.add_field(name=f'{i}. {r_list[i-1]["name"]}',value=f'評價{r_list[i-1]["rating"]}顆星,外送{r_list[i-1]["minimum_delivery_fee"]}元以上,{r_list[i-1]["minimum_delivery_time"]}分鐘送達',inline=False)
+            
             await message.channel.send(embed=embed)
-        
-    elif string.startswith('clear') :
-            used=False
-            await message.channel.send('查詢資料清除成功')
+
+            reply=await message.channel.send("請選擇",components=[
+                SelectMenu(
+                    placeholder="請選所想要的餐廳",
+                    max_values=1,
+                    options=[
+                        SelectOption(f'{1}. {r_list[0]["name"]}','1'),
+                        SelectOption(f'{2}. {r_list[1]["name"]}','2'),
+                        SelectOption(f'{2}. {r_list[2]["name"]}','3'),
+                        SelectOption(f'{4}. {r_list[3]["name"]}','4'),
+                        SelectOption(f'{5}. {r_list[4]["name"]}','5')
+                    ]
+                )
+            ]
+            )
+
+            inter = await reply.wait_for_dropdown()
+            labels = [option.label for option in inter.select_menu.selected_options]
+            await inter.reply(f"已選擇選項: {', '.join(labels)}")
+            
+
+
+
+
 
         
 
@@ -87,4 +105,4 @@ async def on_message(message):
 
 
 load_dotenv()
-bot.run(os.getenv('TOKEN'))
+bot.run('ODc1OTA4MTI4NTQ4NDcwODA1.YRcXAw.-1eXCOXbsBZ778SCMNkmLGSLzlw')
