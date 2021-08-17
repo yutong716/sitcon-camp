7b41b009c9c9b6691b94288671a8acb19f3844dc
楊翔宇 <73743919+racercode@users.noreply.github.com>
2021-08-17 09:14:35 +0800

    新增位址功能

--- get_foodpanda.py
+++ get_foodpanda.py
@@ -1,26 +1,19 @@
-pipenv install dislash.py
-
 import json
 import os
-from os import getenv
-import discord
-import discord as dc
-from discord.ext import commands
 from dislash import SelectMenu, SelectOption, InteractionClient
+import discord as dc
 import asyncio
 import googlemaps
+from discord.ext import commands
 from dotenv import load_dotenv
 import requests as rq
 
-
-
-bot = commands.Bot(command_prefix='##')
-bot = discord.Client()
-
-GOOGLE_API_KEY = getenv('GOOGLE_API_KEY')
-gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
-
+bot=commands.Bot(command_prefix='##')
 slash = InteractionClient(bot)
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
 
 payload={"q":"starbucks","location":{"point":{"latitude":22.6394924,"longitude":120.302583}},"config":"Variant21","customer_id  ":"","vertical_types":["restaurants"],"include_component_types":["vendors"],"include_fields":["feed"],"language_id":"6","opening    _type":"delivery","platform":"web","session_id":"","language_code":"zh","customer_type":"regular","limit":10,"offset":0,"dps_se  ssion_id":"eyJzZXNzaW9uX2lkIjoiMzhhOGUwZTBmYTllZmNlMGIzZTM4ZjNkZDFiMzE4ZjkiLCJwZXJzZXVzX2lkIjoiMTYyODg0ODk0NC42NTQzNzg4MjU2LjQxe    E05NHIxSDgiLCJ0aW1lc3RhbXAiOjE2Mjg5NDYwMDV9","dynamic_pricing":0,"brand":"foodpanda","country_code":"tw","use_free_delivery_la  bel":False}
 
@@ -38,13 +31,13 @@ async def on_ready():
     await channel.send('點餐機器人啟動')
 
 tot=0 # Finished
-box = 0
 user_tot=0 # Finished
 user_num={}# Finished
 user_bought=[]
 user_cost=[]
 dish=[]# Finished
 all_dished=[]
+all_cost=0
 
 
 
@@ -54,42 +47,27 @@ keyword=''
 user=''
 @bot.event
 async def on_message(message):
-    string = message.content
-    global box
-    if message.author == bot.user:
-        return
-    if string.startswith("##init"):
-        string,a=string.split(" ")
-    if a == "我要訂餐":
-        await message.channel.send("請輸入地址:")
-        box=1
-        return
-    if box == 1:
-        address=message.content
-        geocode_result = gmaps.geocode(address)
-        print(geocode_result[0]["geometry"]["location"]["lat"])
-        print(geocode_result[0]["geometry"]["location"]["lng"])
-        await message.channel.send("定位成功")
+    global used,entered,keyword,tot,user_tot,user_num,user_bought,user_cost,dish,all_dished,all_cost
+    string=message.content
     if string.startswith('##init') :
-        string,a,b=string.split(' ')
-        global payload
-        global entered
-        global used
-        global tot
-        global dish
-        global user_tot
+        string,a=string.split(' ')
+        
+        address=a
+        geocode_result = gmaps.geocode(address)
+        a=geocode_result[0]["geometry"]["location"]["lat"]
+        b=geocode_result[0]["geometry"]["location"]["lng"]
         payload['location']['point']['latitude']=float(a)
         payload['location']['point']['longitude']=float(b)
         entered=True
         await message.channel.send('定位成功 !')
     elif string.startswith('##clear') :
-        used = False
+        used=False
+        keyword=''
+        tot=user_tot=all_sost=user_cost=0
+        user_num={}
+        user_bought=all_dished=dish=[]
         await message.channel.send('查詢資料清除成功')
-        tot=0
-        dish_cost=[]
     elif string.startswith('##search') :
-        global keyword
-        global reply
         
         if entered==False :
             await message.channel.send('請先輸入座標')
@@ -165,12 +143,9 @@ async def on_message(message):
 
                 if flag==False :
                     await message.channel.send(embed=em)
+             
         
     elif string.startswith('##buy') :
-        global user_bought
-        global all_dished
-        global user_cost
-        global user_num
         string,a,b=string.split(' ')
         tup=(int(a)-1,b)
         user=message.author
@@ -184,20 +159,18 @@ async def on_message(message):
             user_bought.append([tup])
         await message.channel.send(f'成功購買 !')
         all_dished.append(tup)
+        all_cost+=dish[int(a)-1][0]
 
     elif string.startswith('##check') :
         user=message.author
-        await message.channel.send(f'目前累計{user_cost[user_num[user]]}元，已購買：')
+        embed=dc.Embed(
+            title=f'累計{user_cost[user_num[user]]}元，已購買：'
+            )
         for i in user_bought[user_num[user]] :
-            #print(type(i),type(i[0]))
-            await message.channel.send(f'{dish[i[0]][1]}，註：{i[1]}')
-   
-    elif string.starteith('##end'):
-        user=message.author
-        await message.channel.send(f'總價格:{user_cost[user_num[user]]}元')
-        for i in user_bought[user_num[user]] :
-            await message.channel.send(f'{dish[i[0]][1]}，{i[1]}')
-        await message.channel.send("謝謝購買")
+            embed.add_field(name=f'{dish[i[0]][1]}',value=i[1],inline=False)
+        await message.channel.send(embed=embed)
 
-load_dotenv()
+
+    
+           
 bot.run(os.getenv('TOKEN'))
